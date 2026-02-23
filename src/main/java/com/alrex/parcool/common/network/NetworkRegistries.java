a14fcf791f3acf1f8a1421b91622cff806ccd8aa
BluSpring <bluspringindohq@gmail.com>
2026-02-23 18:12:16 +0800

    Fix dedicated server networking issues

--- src/main/java/com/alrex/parcool/common/network/NetworkRegistries.java
+++ src/main/java/com/alrex/parcool/common/network/NetworkRegistries.java
@@ -10,31 +10,32 @@ public class NetworkRegistries {
 
     public static void onRegisterPayload() {
         PayloadTypeRegistry.playC2S().register(StartBreakfallEventPayload.TYPE, StartBreakfallEventPayload.CODEC);
-        ServerPlayNetworking.registerGlobalReceiver(StartBreakfallEventPayload.TYPE, StartBreakfallEventPayload::handleServer);
         PayloadTypeRegistry.playC2S().register(ActionStatePayload.TYPE, ActionStatePayload.CODEC);
-        ServerPlayNetworking.registerGlobalReceiver(ActionStatePayload.TYPE, ActionStatePayload::handleServer);
         PayloadTypeRegistry.playC2S().register(LimitationPayload.TYPE, LimitationPayload.CODEC);
-        ServerPlayNetworking.registerGlobalReceiver(LimitationPayload.TYPE, LimitationPayload::handleServer);
         PayloadTypeRegistry.playC2S().register(ClientInformationPayload.TYPE, ClientInformationPayload.CODEC);
-        ServerPlayNetworking.registerGlobalReceiver(ClientInformationPayload.TYPE, ClientInformationPayload::handleServer);
         PayloadTypeRegistry.playC2S().register(StaminaPayload.TYPE, StaminaPayload.CODEC);
-        ServerPlayNetworking.registerGlobalReceiver(StaminaPayload.TYPE, StaminaPayload::handleServer);
         PayloadTypeRegistry.playC2S().register(StaminaProcessOnServerPayload.TYPE, StaminaProcessOnServerPayload.CODEC);
+
+        PayloadTypeRegistry.playS2C().register(ActionStatePayload.TYPE, ActionStatePayload.CODEC);
+        PayloadTypeRegistry.playS2C().register(LimitationPayload.TYPE, LimitationPayload.CODEC);
+        PayloadTypeRegistry.playS2C().register(ClientInformationPayload.TYPE, ClientInformationPayload.CODEC);
+        PayloadTypeRegistry.playS2C().register(StaminaPayload.TYPE, StaminaPayload.CODEC);
+        PayloadTypeRegistry.playS2C().register(StaminaProcessOnServerPayload.TYPE, StaminaProcessOnServerPayload.CODEC);
+
+        ServerPlayNetworking.registerGlobalReceiver(StartBreakfallEventPayload.TYPE, StartBreakfallEventPayload::handleServer);
+        ServerPlayNetworking.registerGlobalReceiver(ActionStatePayload.TYPE, ActionStatePayload::handleServer);
+        ServerPlayNetworking.registerGlobalReceiver(LimitationPayload.TYPE, LimitationPayload::handleServer);
+        ServerPlayNetworking.registerGlobalReceiver(ClientInformationPayload.TYPE, ClientInformationPayload::handleServer);
+        ServerPlayNetworking.registerGlobalReceiver(StaminaPayload.TYPE, StaminaPayload::handleServer);
         ServerPlayNetworking.registerGlobalReceiver(StaminaProcessOnServerPayload.TYPE, StaminaProcessOnServerPayload::handleServer);
     }
 
     public static void onRegisterClientPayloads() {
-        PayloadTypeRegistry.playS2C().register(StartBreakfallEventPayload.TYPE, StartBreakfallEventPayload.CODEC);
         ClientPlayNetworking.registerGlobalReceiver(StartBreakfallEventPayload.TYPE, StartBreakfallEventPayload::handleClient);
-        PayloadTypeRegistry.playS2C().register(ActionStatePayload.TYPE, ActionStatePayload.CODEC);
         ClientPlayNetworking.registerGlobalReceiver(ActionStatePayload.TYPE, ActionStatePayload::handleClient);
-        PayloadTypeRegistry.playS2C().register(LimitationPayload.TYPE, LimitationPayload.CODEC);
         ClientPlayNetworking.registerGlobalReceiver(LimitationPayload.TYPE, LimitationPayload::handleClient);
-        PayloadTypeRegistry.playS2C().register(ClientInformationPayload.TYPE, ClientInformationPayload.CODEC);
         ClientPlayNetworking.registerGlobalReceiver(ClientInformationPayload.TYPE, ClientInformationPayload::handleClient);
-        PayloadTypeRegistry.playS2C().register(StaminaPayload.TYPE, StaminaPayload.CODEC);
         ClientPlayNetworking.registerGlobalReceiver(StaminaPayload.TYPE, StaminaPayload::handleClient);
-        PayloadTypeRegistry.playS2C().register(StaminaProcessOnServerPayload.TYPE, StaminaProcessOnServerPayload.CODEC);
         ClientPlayNetworking.registerGlobalReceiver(StaminaProcessOnServerPayload.TYPE, StaminaProcessOnServerPayload::handleClient);
     }
 }
